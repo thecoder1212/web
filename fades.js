9abde79a1af02a3fb499ba75545012629278c12f
thecoder1212 <35535457+thecoder1212@users.noreply.github.com>
2018-07-18 12:00:24 +0530

    Add files via upload

---
+++ fades.js
@@ -0,0 +1,16 @@
+function animate() {
+$('.mainback').hide()
+$('.mainback').fadeIn(1000)
+$('.start').hide()
+$('.start').fadeIn(8000)
+$('.line').hide()
+$('.line').fadeIn(4000)
+$('.idea1').hide()
+$('.idea1').fadeIn(6000)
+$('.idea2').hide()
+$('.idea2').fadeIn(8000)
+$('.idea3').hide()
+$('.idea3').fadeIn(10000)
+
+}
+animate();
